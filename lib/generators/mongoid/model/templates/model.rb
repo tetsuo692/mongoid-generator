fa7e568f15f9c343b561dee1d7a34382b2f26227
guillaume belleguic <guillaume.belleguic@cixi.eu>
2010-04-07 19:20:02 +0200

    fixed each error

--- lib/generators/mongoid/model/templates/model.rb
+++ lib/generators/mongoid/model/templates/model.rb
@@ -3,13 +3,13 @@ class <%= class_name %><%= "< #{options[:parent].classify}" if options[:parent] 
 <% unless options[:parent] -%>
   include Mongoid::Document
   <%= 'include Mongoid::Timestamps' if options[:timestamps] %> 
-  <%=  'include Mongoid::Versioning' if options[:versioning] %>
+  <%= 'include Mongoid::Versioning' if options[:versioning] %>
 <% end -%>
 
-<% attributes.reject{|attr| attr.reference?}.each do |attribute|-%>
+<% attributes.reject{|attr| attr.reference?}.each do |attribute| -%>
   field :<%= attribute.name %>, :type => <%= attribute.type_class %>
 <% end -%>
-<% attributes.select{|attr| attr.reference? } do |attribute| -%>
+<% attributes.select{|attr| attr.reference? }.each do |attribute| -%>
   belongs_to :<%= attribute.name%>, :inverse_of => <%= class_name.pluralize %>
 <% end -%>
 end
