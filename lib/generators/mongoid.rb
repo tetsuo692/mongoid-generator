79600e21bbc82c8894003736c2ea8873d049732e
guillaume belleguic <guillaume.belleguic@cixi.eu>
2010-04-07 13:14:08 +0200

    remove code

--- lib/generators/mongoid.rb
+++ lib/generators/mongoid.rb
@@ -11,50 +11,6 @@ module Mongoid
       end
    
     end
-
-    class ActiveModel < Rails::Generators::ActiveModel #:nodoc:
-      def self.all(klass)
-        "#{klass}.all"
-      end
-
-      def self.find(klass, params=nil)
-        "#{klass}.first(#{params})"
-      end
-
-      def self.build(klass, params=nil)
-        if params
-          "#{klass}.new(#{params})"
-        else
-          "#{klass}.new"
-        end
-      end
-
-      def save
-        "#{name}.save"
-      end
-
-      def update_attributes(params=nil)
-        "#{name}.update(#{params})"  
-      end
-
-      def errors
-        "#{name}.errors"
-      end
-
-      def destroy
-        "#{name}.destroy"
-      end
-    end
   end
 end
 
-module Rails
-  module Generators
-    class GeneratedAttribute #:nodoc:
-      def type_class
-        return 'DateTime' if type.to_s == 'datetime'
-        return type.to_s.camelcase
-      end
-    end
-  end
-end
